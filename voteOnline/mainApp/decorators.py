cd003cf8027e82f39fc1b352c58e45dd450b54f5
2MichaelRD <michael.donahue.r@gmail.com>
2024-11-15 12:16:55 -0500

    csm sched

--- voteOnline/mainApp/decorators.py
+++ voteOnline/mainApp/decorators.py
@@ -106,3 +106,22 @@ def CSM_voter_or_superuser(function):
             return HttpResponseRedirect('/')
 
   return wrap
+
+
+def CSM_schedule_or_superuser(function):
+     @wraps(function)
+     def wrap(request, *args, **kwargs):
+          try:
+               schedule = votingschedule.objects.get(department='CSM')
+               start = schedule.start
+               end = schedule.end
+               today = datetime.datetime.now().date()
+               if today >= start and today <= end or request.user.is_superuser:
+                    return function(request, *args, **kwargs)
+               else:
+                    sweetify.error(request, 'Kindly wait for the schedule!')
+                    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+          except:
+               sweetify.error(request, 'There is no schedule posted yet!')
+               return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+     return wrap
