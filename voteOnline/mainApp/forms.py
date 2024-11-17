b8ab37d4318646c0e70e624ebe91e68d199171f0
2MichaelRD <michael.donahue.r@gmail.com>
2024-11-17 17:53:15 -0500

    CBUS & main

--- voteOnline/mainApp/forms.py
+++ voteOnline/mainApp/forms.py
@@ -44,7 +44,9 @@ class CSM_CandidatesForm(forms.ModelForm):
          'bio':forms.Textarea(attrs={'class': 'form-control', 'placeholder':'Tell us about yourself and your motto' }),
          'position':forms.Select(choices=position_choices,attrs={'class': 'form-control', 'placeholder':'Position' }),
       }
-      
+
+
+
 class CLA_CandidatesForm(forms.ModelForm):
    class Meta:
       model = CLA_Candidate
@@ -52,6 +54,52 @@ class CLA_CandidatesForm(forms.ModelForm):
       fields = ('fullname', 'photo', 'bio', 'position')
       position_choices = (
         ('President','President'),
+         ('Vice_President','Vice_President'),
+         ('Treasurer','Treasurer'),
+         ('Secretary','Secretary'),
+         ('Event_Coordinator','Event_Coordinator'),
+         ('Sports_and_Recreation_Officer','Sports_and_Recreation_Officer'),
+         ('Cultural_affairs_officer','Cultural_affairs_officer'),
+         ('Department_Representative','Department_Representative'),
+      )
+      widgets = {
+         'photo': forms.FileInput(attrs={'type': 'file'}),
+         'fullname':forms.TextInput(attrs={'type': 'text', 'class': 'form-control', 'placeholder':'Full name' }),
+         'bio':forms.Textarea(attrs={'class': 'form-control', 'placeholder':'Tell us about yourself and your motto' }),
+         'position':forms.Select(choices=position_choices,attrs={'class': 'form-control', 'placeholder':'Position' }),
+      }
+
+
+class CBUS_CandidatesForm(forms.ModelForm):
+   class Meta:
+      model = CBUS_Candidate
+      exclude = ('voters',)
+      fields = ('fullname', 'photo', 'bio', 'position')
+      position_choices = (
+         ('President','President'),
+         ('Vice_President','Vice_President'),
+         ('Treasurer','Treasurer'),
+         ('Secretary','Secretary'),
+         ('Event_Coordinator','Event_Coordinator'),
+         ('Sports_and_Recreation_Officer','Sports_and_Recreation_Officer'),
+         ('Cultural_affairs_officer','Cultural_affairs_officer'),
+         ('Department_Representative','Department_Representative'),
+      )
+      widgets = {
+         'photo': forms.FileInput(attrs={'type': 'file'}),
+         'fullname':forms.TextInput(attrs={'type': 'text', 'class': 'form-control', 'placeholder':'Full name' }),
+         'bio':forms.Textarea(attrs={'class': 'form-control', 'placeholder':'Tell us about yourself and your motto' }),
+         'position':forms.Select(choices=position_choices,attrs={'class': 'form-control', 'placeholder':'Position' }),
+      }
+
+
+class MAIN_CandidatesForm(forms.ModelForm):
+   class Meta:
+      model = MAIN_Candidate
+      exclude = ('voters',)
+      fields = ('fullname', 'photo', 'bio', 'position')
+      position_choices = (
+         ('President','President'),
          ('Vice_President','Vice_President'),
          ('Treasurer','Treasurer'),
          ('Secretary','Secretary'),
