838714995f10e95a685118eb52327751ad085a5f
Divyesh Ishvar Ambaliya <divyeshishvarambaliya@Divyeshs-MBP.lan>
2024-11-20 11:04:50 -0500

    updated

--- voteOnline/mainApp/admin.py
+++ voteOnline/mainApp/admin.py
@@ -6,4 +6,16 @@ admin.site.register(votingschedule)
 
 admin.site.register(UserRecords)
 
+admin.site.register(MAIN_Candidate)
+
+admin.site.register(CECS_Candidate)
+
+admin.site.register(CSM_Candidate)
+
+admin.site.register(CLA_Candidate)
+
+admin.site.register(CBUS_Candidate)
+
+
+
 
