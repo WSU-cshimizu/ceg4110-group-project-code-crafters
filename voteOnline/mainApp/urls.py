1b380b44cb92ca46957624055fe82d7703f8d9cf
prashant <rumdali.psn@gmail.com>
2024-11-20 18:59:45 -0500

    urls updated mainApp

--- voteOnline/mainApp/urls.py
+++ voteOnline/mainApp/urls.py
@@ -16,6 +16,21 @@ urlpatterns = [
     path('election/schedule', views.electionschedule, name='electionschedule'),
     path('election/schedule/update/<str:pk>', views.updateelectionschedule, name='updateelectionschedule'),
     path('election/schedule/delete/<str:pk>', views.deleteelectionschedule, name='deleteelectionschedule'),
+    
+    path('main', views.mainballot, name='main'),
+    path('main/candidates', views.maincandidates, name='maincandidates'),
+    path('main/tally', views.maintally, name='maintally'),
+    path('main/result', views.mainresult, name='mainresult'),
+    path('main/candidate/update/<str:pk>', views.updatemaincandidate, name='updatemaincandidate'),
+    path('main/candidate/delete/<str:pk>', views.deletemaincandidate, name='deletemaincandidate'),
+
+    path('CECS/ballot', views.CECSballot, name='CECSballot'),
+    path('CECS/candidates', views.CECScandidates, name='CECScandidates'),
+    path('CECS/tally', views.CECStally, name='CECStally'),
+    path('CECS/result', views.CECSresult, name='CECSresult'),
+    path('CECS/candidate/update/<str:pk>', views.updateCECScandidate, name='updateCECScandidate'),
+    path('CECS/candidate/delete/<str:pk>', views.deleteCECScandidate, name='deleteCECScandidate'),
+
 
     path('CSM/ballot', views.CSMballot, name='CSMballot'),
     path('CSM/candidates', views.CSMcandidates, name='CSMcandidates'),
@@ -31,5 +46,13 @@ urlpatterns = [
     path('CLA/result', views.CLAresult, name='CLAresult'),
     path('CLA/candidate/update/<str:pk>', views.updateCLAcandidate, name='updateCLAcandidate'),
     path('CLA/candidate/delete/<str:pk>', views.deleteCLAcandidate, name='deleteCLAcandidate'),
+
+
+    path('CBUS/ballot', views.CBUSballot, name='CBUSballot'),
+    path('CBUS/candidates', views.CBUScandidates, name='CBUScandidates'),
+    path('CBUS/tally', views.CBUStally, name='CBUStally'),
+    path('CBUS/result', views.CBUSresult, name='CBUSresult'),
+    path('CBUS/candidate/update/<str:pk>', views.updateCBUScandidate, name='updateCBUScandidate'),
+    path('CBUS/candidate/delete/<str:pk>', views.deleteCBUScandidate, name='deleteCBUScandidate'),
     
 ]
